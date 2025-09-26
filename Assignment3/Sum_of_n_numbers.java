b65a1f2d47479a8e6f979e31799893db10e7c51d
jagatprakash Raput <jagat.24scse1180004@galgotiasuniversity.ac.in>
2025-09-26 22:27:19 +0530

    Update Sum_of_n_numbers.java

--- Assignment3/Sum_of_n_numbers.java
+++ Assignment3/Sum_of_n_numbers.java
@@ -1,1 +1,13 @@
-
+public class Sum_of_N_numbers{
+    public static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(sum(n));
+    }
+    
+}
