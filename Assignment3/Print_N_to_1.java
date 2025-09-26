ad70ee02c54fcd6ed2a2deb2e96ce03909909021
jagatprakash Raput <jagat.24scse1180004@galgotiasuniversity.ac.in>
2025-09-26 22:37:58 +0530

    Create Print_N_to_1.java

---
+++ Assignment3/Print_N_to_1.java
@@ -0,0 +1,13 @@
+public class Print_N_to_1 {
+    public static void print(int n){
+        if (n==0){
+            return;
+        }
+        System.out.println(n+" ");
+        print(n-1);
+    }
+    public static void main(String[] args) {
+        int n=10;
+        print(n);
+    }
+}
