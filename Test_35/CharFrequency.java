e1220837510e4593fcc983ba8175f80a1102a7bc
jagatprakash Raput <jagat.24scse1180004@galgotiasuniversity.ac.in>
2025-10-07 22:10:08 +0530

    Create CharFrequency.java

---
+++ Test_35/CharFrequency.java
@@ -0,0 +1,15 @@
+public class CharFrequency {
+    public static void main(String[] args) {
+        String s = "apple";
+        int[] freq = new int[256];
+        for (char c : s.toCharArray())
+            freq[c]++;
+
+        for (char c : s.toCharArray()) {
+            if (freq[c] != 0) {
+                System.out.println(c + " -> " + freq[c]);
+                freq[c] = 0;
+            }
+        }
+    }
+}
